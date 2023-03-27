5f5fea1888601c174c478aed55b88db23b81648c
hyunin3 <stock4share2@gmail.com>
2023-03-27 16:00:21 +0900

    0327

---
+++ 최소합.py
@@ -0,0 +1,26 @@
+dij = [(0,1), (1,0)]
+def dfs(i, j, sm):
+    global ans
+
+    if sm >= ans:
+        return
+
+    if i == N-1 and j == N-1:  #종료조건
+        if sm < ans:
+            ans = sm
+        return
+
+    for di, dj in dij:
+        ni, nj = i + di, j + dj
+
+        if ni > -1 and ni < N and nj > -1 and nj < N:
+            dfs(ni, nj, sm + arr[ni][nj])
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    ans = 1000000
+    dfs(0, 0, arr[0][0]) #첫 줄 제일 왼쪽 칸에서 시작
+
+    print(f'#{tc} {ans}')
