b5f219ef48416f1e37ed1792d714d3ab63f8db9e
hyunin3 <stock4share2@gmail.com>
2023-03-29 19:23:17 +0900

    0329

--- 전기카트.py
+++ 전기카트.py
@@ -1,26 +1,26 @@
+def dfs(n, sm, s):
+    global ans
+    if n==N:
+        # 지금까지의 비용 + 복귀비용
+        ans = min(ans, sm+arr[s][1])
+        return
+ 
+    for j in range(2, N+1):
+        if v[j]==0:
+            v[j]=1
+            dfs(n+1, sm+arr[s][j], j)
+            v[j]=0
+ 
 T = int(input())
-
-# (current, next) = 0,1 -> 1,2 -> 2,0
-def dfs(current, tmp):
-    global res
-    if tmp < res:
-        if 0 not in visited[1:]:  # 전부 방문했다면
-            res = min(res, tmp + a[current][0])  # 결과값 갱신하고 함수 종료
-            return
-        for next in range(1, N):
-            if a[current][next] != 0 and visited[next] == 0:
-                visited[next] = 1
-                dfs(next, tmp + a[current][next])
-                visited[next] = 0
-
-
-for tc in range(1, T+1):
+for test_case in range(1, T + 1):
     N = int(input())
-    a = [list(map(int, input().split())) for _ in range(N)]
-
-    res = 10000
-    for i in range(1, N):
-        visited = [0] * N
-        visited[i] = 1
-        dfs(i, a[0][i])  # current_y, tmp_sum
-    print("#{} {}".format(tc, res))
+    # 위쪽과 왼쪽에 여분공간으로 문제 그림과 번호 맞춤
+    arr = [[0]*(N+1)]+[[0]+list(map(int, input().split())) for _ in range(N)]
+ 
+    v = [0]*(N+1)
+    ans = 100*N
+ 
+    # 방문구역수, 사용량, 시작구역번호
+    dfs(1, 0, 1)
+ 
+    print(f'#{test_case} {ans}')
