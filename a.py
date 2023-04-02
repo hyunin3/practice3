28927e4847da5f20c7f888a168b8283e66aef46a
hyunin3 <stock4share2@gmail.com>
2023-04-02 17:16:32 +0900

    0402

--- a.py
+++ a.py
@@ -1,16 +1,13 @@
+
 def dfs(n, sm):
     global ans
-    
-    if n >= N:
-        return
-    
     if n == N:
         if sm == K:
             ans += 1
         return
-    
-    dfs(n + 1, sm + lst[n])
-    dfs(n + 1, sm)    
+    dfs(n+1, sm+lst[n])     
+    dfs(n+1, sm) 
+
 
 T = int(input())
 for tc in range(1, T + 1):
@@ -19,4 +16,5 @@ for tc in range(1, T + 1):
     ans = 0
     dfs(0, 0)
     
+
     print(f'#{tc} {ans}')
