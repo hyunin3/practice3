83808fbc3454f8f0c3ba50049c890368a828dbf7
hyunin3 <stock4share2@gmail.com>
2023-04-08 15:01:17 +0900

    0408

---
+++ 이진수 변환.py
@@ -0,0 +1,12 @@
+T = int(input())
+for tc in range(1, T + 1):
+    N, st = input().split()
+    lst = []
+    for ch in st:
+        if ch.isdigit():
+            n = ord(ch) - ord('0')
+        else:
+            n = ord(ch) - ord('A') + 10
+        for pos in (3,2,1,0):
+            lst.append((n>>pos)&1)
+    print(f'#{tc} {"".join(map(str, lst))}')                
