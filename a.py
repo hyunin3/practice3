8b5031b618b6bfdb8eaad515ccbccdb882216175
hyunin3 <stock4share2@gmail.com>
2023-04-08 21:27:49 +0900

    0408

--- a.py
+++ a.py
@@ -1,12 +1,12 @@
 T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
-    bits = int(input(), 16)
+    bits = int(input(), 16) 
     ans = cnt = 0
     for i in range(N*4):
         if bits & (1<<i):
             cnt += 1
-            ans = max(ans, cnt)
+            ans = max(ans, cnt)          
         else:
             cnt = 0
-    print(f'#{tc} {ans}')            
+    print(f'#{tc} {ans}')             
